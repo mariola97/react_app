6df0d53818bf07cf7af16e9cd87b529ae0c9b905
mariola97 <maglica.mario68@gmail.com>
2023-02-14 09:25:21 +0100

    api_call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-
+import React, {useState} from "react";
 
 function App() {
+  const url='https://api.openweathermap.org/data/2.5/weather?q=Livno&appid=015a5a01c2d58dc27276745e599fa2d8'
   return (
+    
     <h1>Hello World!1</h1>
   );
 }
