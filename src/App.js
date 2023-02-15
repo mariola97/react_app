0759ca345a8962619a14ff80870b0296ed6f766e
mariola97 <maglica.mario68@gmail.com>
2023-02-15 12:58:07 +0100

    promjena

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-
+import styles from "./index.css";
 function App() {
   const url='https://api.openweathermap.org/data/2.5/weather?q=Livno&appid=015a5a01c2d58dc27276745e599fa2d8'
   return (
@@ -8,11 +8,19 @@ function App() {
         <div className="Header">
           <div className="location">
             <h1>Livno</h1>
+            <p>BA</p>
           </div>
           <div className="temp">
             <h2>8*C</h2>
           </div>
-          <div clas
+          <div className="desc">
+            <p>Clear</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="wind">
+            <p>3.3 m/s</p>
+          </div>
         </div>
       </div>
 
